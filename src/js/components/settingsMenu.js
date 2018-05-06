8e5d4c51f360edd2ec4e81ff6f833736e06098db
james <jamesdaniel2197@gmail.com>
2018-05-06 22:17:02 +0100

    fix issue caused by settingsMenu being open while the user changes route

--- src/js/components/settingsMenu.js
+++ src/js/components/settingsMenu.js
@@ -1,22 +1,52 @@
-import { h } from "preact";
-import { Overlay } from "react-overlays";
+import { h, Component } from "preact";
 
-import FadeTransition from "components/fadeTransition";
+import Transition, { ENTERED, ENTERING, EXITING, EXITED } from "react-transition-group/Transition";
 
-export default function SettingsMenu(props) {
-  return (
-    <Overlay
-      transition={ FadeTransition }
-      show={ props.show }
-      container={ props.container }
-      onHide={ props.onHide }
-      rootClose={true}
-    >
-      <div className={`settingsMenu ${props.className}`}>  
-        { props.children }
-      </div>
-    </Overlay>
-  );
+const DURATION = 200;
+
+const states = {
+  [ENTERING]: 'entering',
+  [ENTERED]: 'entered',
+  [EXITING]: 'exiting',
+  [EXITED]: 'exited',
+};
+
+export default class SettingsMenu extends Component {
+
+  componentWillMount() {
+    this._handleClick = this.handleClick.bind(this);
+    document.addEventListener("click", this._handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("click", this._handleClick, false);
+  }
+
+  render(props, state) {
+    return (
+      <Transition in={props.show} timeout={DURATION}>
+        {(status) => (
+          <div ref={(node) => {this.node = node}} className={`settingsMenu fade ${states[status]}`}>  
+            { props.children }
+          </div>
+        )}
+      </Transition>
+    );
+  }
+
+  handleClick(e) {
+    if (this.node.contains(e.target)) {
+      return;
+    } else {
+      this.handleClickOutside(e);
+    }
+  }
+
+  handleClickOutside(e) {
+    e.stopPropagation();
+    this.props.onHide();
+  }
+
 }
 
 SettingsMenu.defaultProps = {
