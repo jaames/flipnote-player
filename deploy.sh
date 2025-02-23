f796fcf45f388cea613bcf308692b8f3f0e35a57
James <github@jamesdaniel.dev>
2025-02-23 23:00:22 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,10 @@ set -e
 npm run manifest
 npm run build
 
-# navigate into the build output directory
-cd dist
-
-# push to gh-pages branch
+# push to www branch
 git init
-git add -A
+git add dist
 git commit -m 'deploy to web'
-git push -f git@github.com:jaames/flipnote-player.git master:www
+git subtree push --prefix dist origin www
 
 cd -
