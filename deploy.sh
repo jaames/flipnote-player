bfc928ca434b82fa310f08a798c4552fb56a5e3c
James <github@jamesdaniel.dev>
2025-02-23 23:21:33 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,12 @@ npm run manifest
 npm run build
 
 # push to www branch
-git init
-git add dist
-git commit -m 'deploy to web'
-git subtree push --prefix dist origin www
+# src https://stackoverflow.com/questions/48588908/deploying-ignored-dist-folder-to-github-pages
+git commit -am "Save uncommited changes (WIP)"
+git branch --delete --force www
+git checkout --orphan www
+git add -f dist
+git commit -m "Rebuild GitHub pages"
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin www && git checkout -
 
-cd -
+git checkout master
